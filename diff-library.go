df60c8e283a92086529a24da90d4a9ba65c98a71
Garrett <gsweene2@u.rochester.edu>
2022-05-29 13:14:21 -0400

    Diff library example + write_to_file formatting (#1)
    * Diff Library Example
    
    * Better name
    
    * Fix

---
+++ diff-library.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/r3labs/diff"
+)
+
+func DiffLibrary() diff.Changelog {
+	from := make(map[string]map[string]string)
+	item_a_from := map[string]string{"hello": "world"}
+	from["a"] = item_a_from
+	to := make(map[string]map[string]string)
+	item_a_to := map[string]string{"hello": "world1"}
+	to["a"] = item_a_to
+
+	changelog, err := diff.Diff(from, to)
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+	}
+	return changelog
+}
+
+func main() {
+	result := DiffLibrary()
+	fmt.Printf("result: %v\n", result)
+	b, err := json.Marshal(result)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("result as json: %v\n", result)
+	fmt.Println(string(b))
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module garrett-learns-go
 
 go 1.15
+
+require github.com/r3labs/diff v1.1.0 // indirect
