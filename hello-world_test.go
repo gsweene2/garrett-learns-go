aa0e6485166acd72f7ed075b5b5e21e2b3806b7c
Garrett Sweeney <garrett.d.sweeney@gmail.com>
2022-05-29 13:17:27 -0400

    fo fmt .

--- hello-world_test.go
+++ hello-world_test.go
@@ -3,11 +3,10 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-    got := Hello()
-    want := "Hello, Garrett"
+	got := Hello()
+	want := "Hello, Garrett"
 
-    if got != want {
-        t.Errorf("got %q want %q", got, want)
-    }
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
-
